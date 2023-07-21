cad3b8ff48e59dca015635f7211bf55760475fdc
yiftachmeitar <yimeitar@gmail.com>
2023-07-21 15:19:31 +0300

    Works with 2 problems: 1. popup doesn't show 2. request for vehicles is multiplied

--- fleet-manager/src/MapComponent.js
+++ fleet-manager/src/MapComponent.js
@@ -3,15 +3,19 @@ import { MapContainer, TileLayer, Polygon, FeatureGroup, Marker } from "react-le
 import { LatLngExpression } from "leaflet";
 import { EditControl } from "react-leaflet-draw";
 import axios from "axios";
+import Modal from 'react-modal';
 
 const defaultPosition: LatLngExpression = [51.505, -0.09];
+Modal.setAppElement('#root');
 
 const MapComponent = () => {
   const [vehicles, setVehicles] = useState([]); // Define vehicles and setVehicles
+  const [selectedVehicles, setSelectedVehicles] = useState([]); // Define selectedVehicles and setSelectedVehicles
   const [polygon, setPolygon] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   useEffect(() => {
-    axios.get('http://127.0.0.1:5000/vehicles').then(response => {
+      axios.get('http://127.0.0.1:5000/vehicles').then(response => {
       setVehicles(response.data); // Update this to use setVehicles
     });
   }, []);
@@ -21,8 +25,14 @@ const MapComponent = () => {
     let points = layer.getLatLngs(); // getting the coordinates of the polygon
     setPolygon(points[0]); // since getLatLngs() returns multi-dimensional array, take the first array
     axios.post('http://127.0.0.1:5000/vehicles/polygon', points[0]).then(response => {
-      setVehicles(response.data); // Update this to use setVehicles
+      setSelectedVehicles(response.data);
+      setModalIsOpen(true);
     });
+  }
+
+  const handleModalClose = () => {
+      setModalIsOpen(false);
+      setPolygon([]);
   }
 
   return (
@@ -45,21 +55,35 @@ const MapComponent = () => {
           </FeatureGroup>
           {polygon && <Polygon positions={polygon} />}
           {vehicles.map((vehicle, index) => (
-            <Marker key={index} position={[vehicle.lat, vehicle.lng]} /> // Represent vehicles as markers on the map
+            <Marker key={index} position={[vehicle.lat, vehicle.lng]} />
           ))}
         </MapContainer>
       </div>
-      <div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        ariaHideApp={false}
+        style={{ // Optional styles
+          content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)'
+          }
+        }}
+      >
         <h2>Vehicles in Selected Area:</h2>
         <ul>
-          {vehicles.map((vehicle, index) => (
-            <li key={index}>Lat: {vehicle.lat}, Lng: {vehicle.lng}</li>
+          {selectedVehicles.map((vehicle, index) => (
+            <li key={index}>{vehicle.id}</li>
           ))}
         </ul>
-      </div>
+        <button onClick={handleModalClose}>Close</button>
+      </Modal>
     </div>
   );
-
 };
 
 export default MapComponent;
