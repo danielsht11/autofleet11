f448b1949c930aca7fe658d345b6bcc980dc4a04
daniel.steinberg <daniel.steinberg@watchful.ai>
2023-07-21 13:52:37 +0300

    solving errors

--- backend/resources/vehicle.py
+++ backend/resources/vehicle.py
@@ -3,8 +3,8 @@ from flask.views import MethodView
 from flask_smorest import Blueprint
 import json
 from os import path
-from backend.schemas import PlainVehicleSchema, VehicleLocationSchema
-from backend.root import SITE_ROOT
+from schemas import PlainVehicleSchema, VehicleLocationSchema
+from root import SITE_ROOT
 from shapely import Polygon, Point
 
 blp = Blueprint("Vehicles", "vehicles", description="Operations on vehicles")
